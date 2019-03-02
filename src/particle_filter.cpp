af17f3c6c09374aef9d5ef7b1d07430e3f661f07
Seo, Jungwoo <36559904+imisu9@users.noreply.github.com>
2019-03-02 17:20:29 +0900

    fix: replace find_if()

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -173,11 +173,20 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
       double t_ob_x = transformed_obs[l].x;
       double t_ob_y = transformed_obs[l].y;
       int landmark_id = transformed_obs[l].id;
+      /*
       vector<LandmarkObs>::iterator it = std::find_if(predictions.begin(), predictions.end(),
                                                       [&landmark_id] (const LandmarkObs lm_ob) {return lm_ob.id == landmark_id;});
       double p_x = it->x;
       double p_y = it->y;
       std::cout << "p_x " << p_x << "\n";
+      */
+      
+      for (unsigned m = 0; m < preditions.size(); ++m) {
+        if (predictions[m].id == landmark_id) {
+          p_x = predictions[m].x;
+          p_y = predictions[m].y;
+        }
+      }
       
       temp_weight *= (1/(2*M_PI*std_landmark[0]*std_landmark[1]) * 
                       exp((-1/2)*(pow(t_ob_x-p_x, 2)/pow(std_landmark[0], 2) + 
